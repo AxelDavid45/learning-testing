3ce8f7b48a6b21cf859f8950f7a3b91abe207122
Axel Espinosa <axeldavid45@gmail.com>
2020-05-04 22:18:45 -0500

    Agregamos archivo configuracion xml y primer test

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Calculator;
+class CalculatorTest extends TestCase
+{
+    public function test_sum()
+    {
+        $calculator = new Calculator();
+        $this->assertEquals(5, $calculator->sum(3, 2));
+    }
+}
