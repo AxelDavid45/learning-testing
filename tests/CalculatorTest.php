23c355a8d5154796110af90339e9fd1c8456340e
Axel Espinosa <axeldavid45@gmail.com>
2020-05-04 22:41:40 -0500

    Agregamos verificaciones para array, clase

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -8,11 +8,18 @@ class CalculatorTest extends TestCase
     {
         $calculator = new Calculator();
         $this->assertEquals(5, $calculator->sum(3, 2));
+
+        //Verify the type of class
+        $this->assertInstanceOf(Calculator::class, $calculator);
     }
 
     public function test_asserts()
     {
+        //Verify if the class contains an attribute
         $this->assertClassHasAttribute('operator', Calculator::class);
+
+        //Verify if an element exists in an array
+        $this->assertContains(5, [4,5,3,2,8]);
 
     }
 }
